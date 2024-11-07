f47c77e9e4eb0bde7752c53e21bfc953fe7b728d
Tolik9924 <sakurasoft9924@gmail.com>
2024-11-07 02:51:40 +0200

    Make Create Test Form

--- src/pages/CreateTest/CreateTest.tsx
+++ src/pages/CreateTest/CreateTest.tsx
@@ -1,113 +1,280 @@
 import React, { useState } from 'react';
 
-import { Button, Input } from 'antd';
+import { Button, Checkbox, Input } from 'antd';
 
 import { useForm, SubmitHandler, Controller } from "react-hook-form"
 
 import styles from './CreateTest.module.css';
 
-interface SaveTest {
-  sentence: string,
-  ukrainian: string,
+type Answer = {
+  id: number;
+  name: string;
+  isCorrect: boolean;
+}
+
+type Question = {
+  id: number;
+  answers: Array<Answer>
+}
+
+type Sentence = {
+  id: number;
+  english: string;
+  ukrainian: string;
+  questions?: Array<Question>;
+}
+
+type Test = {
+  topic: string,
+  sentences: Array<Sentence>
 }
 
 const CreateTest = () => {
-  const [questions, setQuestions] = useState([
-    {
-      id: 1,
-      name: '1 Question',
-      answers: [
-        {
-          id: 1,
-          name: "1 answer",
-        }
-      ]
-    }
-  ]);
-  const { control, handleSubmit, formState: { errors } } = useForm<SaveTest>({
-    defaultValues: {
-      sentence: "",
-      ukrainian: "",
-    }
+  const [test, setTest] = useState<Test>({
+    topic: "",
+    sentences: [
+      {
+        id: 1,
+        english: "",
+        ukrainian: "",
+        answers: []
+      } as Sentence
+    ],
   });
 
-  const onSubmit: SubmitHandler<SaveTest> = (data) => {
-    console.log("Data: ", data)
-  }
+  const [topic, setTopic] = useState<string>("");
+  const [sentences, setSentences] = useState([
+    {
+      id: 1,
+      english: "",
+      ukrainian: ""
+    }
+  ]);
+
+  const [questions, setQuestions] = useState([{
+    id: 1,
+    answers: [{
+      id: 1,
+      name: "",
+      isCorrect: false
+    }]
+  }]);
+
+  const changeTopic = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTopic(event.target.value);
+  };
+
+  const changeEnglish = (id: number, event: React.ChangeEvent<HTMLInputElement>) => {
+    setSentences([...sentences.map((item) => {
+      if (id === item.id) {
+        return { ...item, english: event.target.value };
+      } else {
+        return { ...item };
+      }
+    })]);
+  };
+
+  const changeUrkainian = (id: number, event: React.ChangeEvent<HTMLInputElement>) => {
+    setSentences([...sentences.map((item) => {
+      if (id === item.id) {
+        return { ...item, ukrainian: event.target.value };
+      } else {
+        return { ...item };
+      }
+    })]);
+  };
+
+  const changeAnswer = (idQuestion, idAnswer, event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestions([...questions.map((question) => {
+      if (question.id === idQuestion) {
+        return {
+          ...question,
+          answers: [
+            ...question.answers.map((answer) => {
+              if (answer.id === idAnswer) {
+                return {
+                  ...answer,
+                  name: event.target.value
+                } as Answer;
+              } else {
+                return { ...answer } as Answer;
+              }
+            })
+          ]
+        } as Question
+      } else {
+        return { ...question } as Question
+      }
+    })]);
+  };
+
+  const changeIsTrueAnswer = (idQuestion, idAnswer) => {
+    setQuestions([...questions.map((question) => {
+      if (question.id === idQuestion) {
+        return {
+          ...question,
+          answers: [
+            ...question.answers.map((answer) => {
+              if (answer.id === idAnswer) {
+                return {
+                  ...answer,
+                  isCorrect: !answer.isCorrect
+                } as Answer;
+              } else {
+                return { ...answer } as Answer;
+              }
+            })
+          ]
+        } as Question
+      } else {
+        return { ...question } as Question
+      }
+    })]);
+  };
+
+  const addSentence = () => {
+    setSentences([...sentences, {
+      id: sentences.length + 1,
+      english: "",
+      ukrainian: ""
+    }]);
+    setQuestions([...questions, {
+      id: questions.length + 1,
+      answers: [{
+        id: 1,
+        name: "",
+        isCorrect: false
+      }]
+    }]);
+  };
+
+  const addAnswer = (idQuestion) => {
+    setQuestions([...questions.map((question) => {
+      if (question.id === idQuestion) {
+        return {
+          ...question,
+          answers: [...question.answers, {
+            id: question.answers.length + 1,
+            name: "",
+            isCorrect: false
+          }]
+        }
+      } else {
+        return { ...question };
+      }
+    })]);
+  };
+
+  const saveTest = () => {    
+    setTest( {
+      ...test,
+      topic: topic,
+      sentences: [...sentences.map((sentence) => {
+        return {
+          ...sentence,
+          questions: questions.find((question) => question.id === sentence.id)
+        } as Sentence})]
+    });
+  };
+
+  console.log("Topic: ", topic);
+  console.log("Sentences: ", sentences);
+  console.log("Questions: ", questions);
+  console.log("Test: ", test);
 
   return (
-    <div className={styles.container}>
-      <h1 className={styles.header}>Create Sentence</h1>
-      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-        <div className={styles.formItem}>
-          <p className={styles.nameOfItem}>English: </p>
-          <Controller
-            name="sentence"
-            control={control}
-            rules={{ 
-              required: 'Input english sentence.',
-              minLength: {
-                value: 5,
-                message: "More than 5 characters."
-              },
-              maxLength: {
-                value: 30,
-                message: "No more than 30 characters."
-              },
-              pattern: {
-                value: /[A-za-z][?|!|.]/,
-                message: "Must be English letters and in the end of sentence ! or ? or ."
-              }  
-            }}
-            render={({ field }) => <Input size='large' {...field} />}
-          />
+    <div>
+      <div className={styles.container}>
+        <div className={styles.form}>
+          <div className={styles.formItem}>
+            <p className={styles.nameOfItem}>Topic:</p>
+            <div className={styles.inputContainer}>
+              <Input value={topic} onChange={changeTopic} size="large" />
+            </div>
+          </div>
+          {sentences.map((item) => (
+            <div key={item.id}>
+              <div className={styles.formItem}>
+                <p className={styles.nameOfItem}>Ukrainian:</p>
+                <div className={styles.inputContainer}>
+                  <Input
+                    value={item.ukrainian}
+                    onChange={(event) => changeUrkainian(item.id, event)}
+                    size="large"
+                  />
+                </div>
+              </div>
+              <div key={item.id} className={styles.formItem}>
+                <p className={styles.nameOfItem}>English:</p>
+                <div className={styles.inputContainer}>
+                  <Input
+                    value={item.english}
+                    onChange={(event) => changeEnglish(item.id, event)}
+                    size="large"
+                  />
+                </div>
+              </div>
+              {questions.map((question) => {
+                if (question.id === item.id) {
+                  return (
+                    question.answers.map((item) => (
+                      <div key={item.id}>
+                        <div className={styles.test}>
+                          <p className={styles.nameOfTest}>Answer: </p>
+                          <div className={styles.inputAnswerContainer}>
+                            <Input
+                              value={item.name}
+                              onChange={(event) => changeAnswer(question.id, item.id, event)}
+                              size="middle"
+                            />
+                          </div>
+                          <Checkbox
+                            checked={item.isCorrect}
+                            onChange={() => changeIsTrueAnswer(question.id, item.id)}
+                          />
+                        </div>
+                        {
+                          item.id === question.answers.length
+                          && <div className={styles.addAnswer}>
+                            <Button
+                              size='small'
+                              type='primary'
+                              onClick={() => addAnswer(question.id)}
+                            >
+                              Add Answer
+                            </Button>
+                          </div>
+                        }
+                      </div>
+                    ))
+                  );
+                } else {
+                  return <p key={question.id}></p>;
+                }
+              })}
+            </div>
+          ))}
+          <div className={styles.addContainer}>
+            <Button
+              onClick={addSentence}
+              size='middle'
+              type='primary'
+            >
+              Add Sentence
+            </Button>
+          </div>
+          <div className={styles.saveTest}>
+            <Button 
+              size="large"
+              type='primary'
+              block
+              onClick={saveTest}
+            >
+              Save Test
+            </Button>
+          </div>
         </div>
-        {errors.sentence && (
-          <p className={styles.errorMessage}>{errors.sentence.message}</p>
-        )}
-        <div className={styles.formItem}>
-          <p className={styles.nameOfItem}>Ukrainian: </p>
-          <Controller
-            name="ukrainian"
-            control={control}
-            rules={{ 
-              required: 'Введіть перекладену відповідь.',
-              minLength: {
-                value: 5,
-                message: "Більше ніж 5 літер.",
-              },
-              maxLength: {
-                value: 30,
-                message: "Не більше ніж 30 літер."
-              },
-              pattern: {
-                value: /[А-Яа-я][?|!|.]/,
-                message: "Повинні бути українські літери та в кінці речення! чи ? або ."
-              } 
-            }}
-            render={({ field }) => <Input size='large' {...field} />}
-          />
-        </div>
-        {errors.sentence && (
-          <p className={styles.errorMessage}>{errors.ukrainian?.message}</p>
-        )}
-        <Button
-          size="middle"
-          type='primary'
-        >
-          Add Question
-        </Button>
-        <div className={styles.saveTestContainer}>
-          <Button
-            size="large"
-            type='primary'
-            htmlType='submit'
-            block
-          >
-            Save Test
-          </Button>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
