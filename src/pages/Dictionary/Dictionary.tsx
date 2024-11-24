1fe9976e4a93ee16afaa71f6c0c1abcd0b7101fe
Tolik9924 <sakurasoft9924@gmail.com>
2024-11-24 02:41:42 +0200

    Make Logic For Dictionary Page

--- src/pages/Dictionary/Dictionary.tsx
+++ src/pages/Dictionary/Dictionary.tsx
@@ -45,7 +45,7 @@ const startTest = [
   {
     id: 3,
     sentence: "Я розумію.",
-    correctAnswer: "I understand.",
+    correctAnswer: "I understand",
   }
 ];
 
@@ -53,8 +53,8 @@ const Dictionary = () => {
   const [answers, setAnswers] = useState<Answer[]>([]);
   const [chooseAnswer, setChooseAnswer] = useState<string>("");
 
-  // From Lesson Page
   const [isCorrect, setIsCorrect] = useState(false);
+  const [isCorrectAnswer, setIsCorrectAnswer] = useState(0);
 
   const [test, setTest] = useState<Test[]>([]);
   const [sentence, setSentence] = useState({ ...startTest[0] });
@@ -63,31 +63,45 @@ const Dictionary = () => {
 
   const [testId, setTestId] = useState<number>(1);
   const [wordId, setWordId] = useState(0);
+  const [answeredQuestions, setAnsweredQuestions] = useState(0);
 
   const fetchDefinition = async (word: string) => {
+    console.log("WORD IN FETCH DEFINITION: ", word);
+
     const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     try {
       const response = await axios.get(url);
       console.log("RESPONSE DATA: ", response.data);
       if (response.data[0].meanings[0].synonyms.length !== 0) {
-        setAnswers(response.data[0].meanings[0].synonyms);
+        await takeAnswersFromDefinition(response.data[0].meanings[0].synonyms, word, 'synonyms');
       } else if (response.data[0].meanings[0].antonyms.length !== 0) {
         setAnswers(response.data[0].meanings[0].antonyms);
+        await takeAnswersFromDefinition(response.data[0].meanings[0].antonyms, word, 'antonyms');
       } else {
-        takeAnswersFromDefinition(response.data[0].meanings[0].definitions);
+        await takeAnswersFromDefinition(response.data[0].meanings[0].definitions, word, 'definitions');
       }
     } catch (error) {
       console.error('Error fetching definition:', error);
     }
   };
 
-  const takeAnswersFromDefinition = (data) => {
-    const result: Answer[] = data[0].definition
+  const takeAnswersFromDefinition = (data, correctAnswer: string, type: string) => {
+    if (type === 'definitions') {
+      const result: Answer[] = data[0].definition
       .split(" ")
       .filter(((word) => !word.includes("(") && !word.includes(")")))
+      .filter((word, i, self) => i === self.indexOf(word) && word !== correctAnswer)
       .map((item, index) => ({ id: index + 1, name: item, isCorrect: false }));
-    // DELETE DUBLICATE OBJECT
-    setAnswers(shuffle([...result, { id: result.length + 1, name: correctAnswers[0], isCorrect: true }]));
+
+    setAnswers(shuffle([...result, { id: result.length + 1, name: correctAnswer, isCorrect: true }]));
+    } 
+    
+    if (type === 'synonyms' || type === 'antonyms') {
+      const result = data
+        .map((item, index) => ({ id: index + 1, name: item, isCorrect: false }));
+      
+      setAnswers(shuffle([...result, { id: result.length + 1, name: correctAnswer, isCorrect: true }]));
+    }
   };
 
   const shuffle = (array: Answer[]) => {
@@ -104,22 +118,49 @@ const Dictionary = () => {
   };
 
   const onClick = (answer: Answer) => {
-    console.log("WORD: ", answer);
+    const answered = answeredQuestions + 1;
+    let correct = 0;
+    setAnsweredQuestions(answered);
+
     if (answer.isCorrect) {
-      console.log("ANSWER IS CORRECT");
-    } else {
-      console.log("ANSWER IS NOT CORRECT");
+      correct = isCorrectAnswer + 1;
+      setIsCorrectAnswer(correct);
     }
-    setChooseAnswer(chooseAnswer + ` ${answer.name}`);
-    const word = wordId + 1;
-    setWordId(word + 1);
-    setAnswer(correctAnswers[word]);
-    fetchDefinition(correctAnswers[word]);
+
+    if (answered !== correctAnswers.length) {
+      const word = wordId + 1;
+      setWordId(word);
+      setAnswer(correctAnswers[word]);
+      fetchDefinition(correctAnswers[word]);
+    }
+
+    if (answered === correctAnswers.length) {
+      setAnswers([]);
+      if (correct === correctAnswers.length) {
+        setIsCorrect(true);
+      }
+
+      if (correct !== correctAnswers.length) {
+        setIsCorrect(false);
+      }
+    }
+
+  setChooseAnswer(chooseAnswer + ` ${answer.name}`);
   };
 
-  console.log("ANSWERS: ", answers);
-  console.log("Correct Answers: ", correctAnswers);
-  console.log("ANSWER: ", answer);
+  const showSentence = (id: number) => {
+    setChooseAnswer("");
+    for (let i = 0; i < startTest.length; i++) {
+      if (startTest[i].id === id) {
+        setSentence({ ...startTest[i] });
+        setCorrectAnswers(startTest[i].correctAnswer.split(" "));
+        setAnswer(startTest[i].correctAnswer.split(" ")[0]);
+        fetchDefinition(startTest[i].correctAnswer.split(" ")[0]);
+        setWordId(0);
+        setAnsweredQuestions(0);
+      }
+    }
+  };
 
   useEffect(() => {
     fetchDefinition(answer);
@@ -127,7 +168,9 @@ const Dictionary = () => {
     setTest([...allQuestions]);
   }, []);
 
-  console.log("TEST: ", test);
+  useEffect(() => {
+    showSentence(testId);
+  }, [testId]);
 
   return (
     <div className={styles.lessonPage}>
@@ -155,8 +198,8 @@ const Dictionary = () => {
         <div className={styles.answerContainer}>
           {isCorrect
             ? <div className={styles.uncorrectAnswerContainer}>
-              <p className={styles.uncorrectAnswer}>{answer}</p>
-              <p className={styles.answer}>{sentence.correctAnswer}</p>
+              <p className={styles.uncorrectAnswer}>{sentence.correctAnswer}</p>
+              <p className={styles.answer}>You Complete Test.</p>
             </div>
             : <div className={styles.correctAnswerContainer}>
               <p className={styles.answer}>{chooseAnswer}</p>
@@ -182,6 +225,19 @@ const Dictionary = () => {
             );
           })
         }
+      </div>
+      <div className={styles.pagination}>
+        {test.map((item) => (
+          <Button
+            key={item.id}
+            size="s"
+            variant={item.isCompleted ? "success" : "primary"}
+            onclick={() => {
+              setTestId(item.id)
+            }}>
+            {item.id}
+          </Button>
+        ))}
       </div>
     </div>
   );
