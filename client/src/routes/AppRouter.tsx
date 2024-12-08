621dc6671c11c7829157789f91188efa2a37eb4a
Tolik9924 <sakurasoft9924@gmail.com>
2024-12-08 22:20:06 +0200

    Make UI Form For Profile

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -5,6 +5,7 @@ import Lesson from '../pages/Lesson/Lesson.tsx';
 import CreateTest from '../pages/CreateTest/CreateTest.tsx';
 import Dictionary from '../pages/Dictionary/Dictionary.tsx';
 import Chat from '../pages/Chat/Chat.tsx';
+import Profile from '../pages/Profile/Profile.tsx';
 
 const RouterWrapper = () => {
   return (
@@ -14,6 +15,7 @@ const RouterWrapper = () => {
       <Route path="/lesson/:id" element={<Lesson />} />
       <Route path="/dictionary/:id" element={<Dictionary />} />
       <Route path="/create-test" element={<CreateTest />} />
+      <Route path="/profile" element={<Profile />} />
     </Routes>
   );
 };
